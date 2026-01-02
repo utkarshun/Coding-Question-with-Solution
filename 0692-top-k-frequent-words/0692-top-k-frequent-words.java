fb6a98bf1392c9f0d552ef006779bfd34ff677dd
Utkarsh kher <136150513+utkarshun@users.noreply.github.com>
2026-01-02 13:43:18 +0530

    Time: 8 ms (56.8%), Space: 46.8 MB (10.15%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,49 @@
+// class Solution {
+//     public List<String> topKFrequent(String[] words, int k) {
+//         Map<String,Integer>map=new HashMap<>();
+//         for(String w:words){
+//             map.put(w,map.getOrDefault(w,0)+1);
+//         }
+//         List<String>list=new ArrayList<>(map.keySet());
+//         Collections.sort(list,(a,b)->{
+//             if(map.get(a).equals(map.get(b)))
+//                 return a.compareTo(b);
+//             return map.get(b)-map.get(a);
+//         });
+//         return list.subList(0,k);
+
+//     }
+// }
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+
+        Map<String, Integer> freq = new HashMap<>();
+        for (String w : words) {
+            freq.put(w, freq.getOrDefault(w, 0) + 1);
+        }
+
+        PriorityQueue<String> pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (!freq.get(a).equals(freq.get(b))) {
+                    return freq.get(a) - freq.get(b);
+                }
+                return b.compareTo(a);
+            }
+        );
+
+        for (String w : freq.keySet()) {
+            pq.offer(w);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        List<String> res = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            res.add(pq.poll());
+        }
+
+        Collections.reverse(res);
+        return res;
+    }
+}
