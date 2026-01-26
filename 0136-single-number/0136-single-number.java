46f0bc33049b6324df802d86e3dc10fd3c650977
Utkarsh kher <136150513+utkarshun@users.noreply.github.com>
2026-01-26 19:02:39 +0530

    Time: 1 ms (99.85%), Space: 47 MB (37.97%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,23 +1,23 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])==1){
-                return nums[i];
-            }
-        }
-        return -1;
-    }
-}
 // class Solution {
 //     public int singleNumber(int[] nums) {
-//         int xor=0;
+//         HashMap<Integer,Integer>map=new HashMap<>();
 //         for(int i=0;i<nums.length;i++){
-//             xor=xor^nums[i];
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
 //         }
-//         return xor;
+//         for(int i=0;i<nums.length;i++){
+//             if(map.get(nums[i])==1){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
 //     }
 // }
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor=xor^nums[i];
+        }
+        return xor;
+    }
+}
