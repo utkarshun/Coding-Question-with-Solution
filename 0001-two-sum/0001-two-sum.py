dcb3dc3aa4da4ed03e05d153f318964e0308ca78
Utkarsh kher <136150513+utkarshun@users.noreply.github.com>
2026-01-26 19:15:32 +0530

    Time: 3 ms (51.4%), Space: 20.8 MB (7.73%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr=[(num,i) for i,num in enumerate(nums)]
+        arr.sort()
+        left,right=0,len(nums)-1
+        while left<right:
+            s=arr[left][0]+arr[right][0]
+            if s==target:
+                return [arr[left][1],arr[right][1]]
+            elif s<target:
+                left+=1
+            else:
+                right-=1
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         mp={}
+#         for i,num in enumerate(nums):
+#             diff=target-num
+#             if diff in mp:
+#                 return [mp[diff],i]
+#             mp[num]=i
