9a261129468de68125773c17679ed16e37bfdc02
Utkarsh kher <136150513+utkarshun@users.noreply.github.com>
2026-01-28 00:04:25 +0530

    Time: 1 ms (99.66%), Space: 94.6 MB (23.49%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,33 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int diff=0;
+//         int maxi=Integer.MIN_VALUE;
+//         int n=prices.length;
+//         if(prices.length<2) return 0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<prices.length;j++){
+//                 diff=prices[j]-prices[i];
+//                 if(diff<0) diff=0;
+//                 maxi=Math.max(maxi,diff);
+//             }
+//         }
+//         return maxi;
+//     }
+// }
+class Solution {
+    public int maxProfit(int[] prices) {
+        int diff=0;
+        int maxi=0;
+        int i=0;
+        for(int j=1;j<prices.length;j++){
+            if(prices[i]<prices[j]){
+                diff=prices[j]-prices[i];
+                maxi=Math.max(diff,maxi);
+            }
+            else{
+                i=j;
+            }
+        }
+        return maxi;
+    }
+}
