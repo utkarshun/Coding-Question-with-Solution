40501aead6f2a3daab3d785e6a9a68fecd3df76f
Utkarsh kher <136150513+utkarshun@users.noreply.github.com>
2026-02-20 20:43:49 +0530

    Time: 13 ms (19.8%), Space: 46.9 MB (64.68%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])==1){
-                return nums[i];
+        for(int num:nums){
+            if(map.get(num)==1){
+                return num;
             }
         }
         return -1;
