710ff893c68b4eb8aa96ed43f47d8c384675af9f
Utkarsh kher <kherutkarsh54@gmail.com>
2026-03-01 11:46:28 +0530

    Time: 1 ms (96.44%) | Memory: 43 MB (83.95%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,11 +1,31 @@
+// class Solution {
+//     public int minimumOperations(int[] nums) {
+//         HashSet<Integer>temp=new HashSet<>();
+//         for(int num:nums){
+//             if(num!=0){
+//                 temp.add(num);
+//             }
+//         }
+//         return temp.size();
+//     }
+// }
 class Solution {
     public int minimumOperations(int[] nums) {
-        HashSet<Integer>temp=new HashSet<>();
-        for(int num:nums){
-            if(num!=0){
-                temp.add(num);
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int n:nums){
+            if(n>0){
+                pq.add(n);
             }
         }
-        return temp.size();
+        int operation=0;
+        int prev=0;
+        while(!pq.isEmpty()){
+            int curr=pq.poll();
+            if(curr!=prev){
+                operation++;
+                prev=curr;
+            }
+        }
+        return operation;
     }
 }
