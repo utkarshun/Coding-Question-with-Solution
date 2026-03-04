c09506191c999aaec131fc4b671de326949752ef
Utkarsh kher <kherutkarsh54@gmail.com>
2026-03-04 10:31:40 +0530

    Time: 0 ms (100%), Space: 19.3 MB (91.23%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,35 @@
+# class Solution:
+#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+#         # sort(nums)
+#         ans=[]
+#         res=set()
+#         n=len(nums)
+#         def subsetWithDupHelper(ind:int,ds:List[int]):
+#             if index==len(nums):
+#                 ds.sort()
+#                 res.add(tuple(ds))
+#                 return
+#             ds.append(nums[ind])
+#             subsetWithDupHelper(ind+1,ds)
+#             ds.pop()
+#             subsetWithDupHelper(ind+1,ds)
+#         subsetWithDupHelper(0,[])
+#         for it in res:
+#             ans.append(list(it))
+#         return ans
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result=[]
+        self.helper(nums,0,[],result)
+        return result
+    def helper(self,nums,ind,temp,result):
+        if ind==len(nums):
+            result.append(temp[:])
+            return
+        temp.append(nums[ind])
+        self.helper(nums,ind+1,temp,result)
+        temp.pop()
+        while ind+1<len(nums) and nums[ind]==nums[ind+1]:
+            ind+=1
+        self.helper(nums,ind+1,temp,result)
