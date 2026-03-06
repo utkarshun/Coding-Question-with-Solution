a779151e621fac04c91d9ae719e360359ade471a
Utkarsh kher <kherutkarsh54@gmail.com>
2026-03-07 00:09:55 +0530

    Time: 0 ms (100%), Space: 19.4 MB (84.14%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -17,19 +17,33 @@
 #         for it in res:
 #             ans.append(list(it))
 #         return ans
+# class Solution:
+#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()
+#         result=[]
+#         self.helper(nums,0,[],result)
+#         return result
+#     def helper(self,nums,ind,temp,result):
+#         if ind==len(nums):
+#             result.append(temp[:])
+#             return
+#         temp.append(nums[ind])
+#         self.helper(nums,ind+1,temp,result)
+#         temp.pop()
+#         while ind+1<len(nums) and nums[ind]==nums[ind+1]:
+#             ind+=1
+#         self.helper(nums,ind+1,temp,result)
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        result=[]
-        self.helper(nums,0,[],result)
-        return result
-    def helper(self,nums,ind,temp,result):
-        if ind==len(nums):
-            result.append(temp[:])
-            return
-        temp.append(nums[ind])
-        self.helper(nums,ind+1,temp,result)
-        temp.pop()
-        while ind+1<len(nums) and nums[ind]==nums[ind+1]:
-            ind+=1
-        self.helper(nums,ind+1,temp,result)
+        res=[]
+        self.helper(0,nums,[],res)
+        return res
+    def helper(self,ind,nums,temp,res):
+        res.append(temp[:])
+        for i in range(ind,len(nums)):
+            if (i>ind and nums[i]==nums[i-1]):
+                continue
+            temp.append(nums[i])
+            self.helper(i+1,nums,temp,res)
+            temp.pop()
