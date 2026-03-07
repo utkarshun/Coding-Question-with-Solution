e8e9837607849d77d5c0ffe33c0f9cbd5823b852
Utkarsh kher <kherutkarsh54@gmail.com>
2026-03-07 09:51:05 +0530

    Time: 6 ms (97.27%) | Memory: 19.7 MB (65.89%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,19 +1,5 @@
 class Solution:
-    def check(self,n,board,i,j):
-        row,col=i,j
-        while row>=0 and col>=0:
-            if board[row][col]=='Q':
-                return False
-            row-=1
-            col-=1
-        row,col=i,j
-        while row>=0 and col<n:
-            if board[row][col]=='Q':
-                return False
-            row-=1
-            col+=1
-        return True
-    def find(self,row,n,ans,board,column):
+    def find(self,row,n,ans,board,column,leftDig,rightDig):
         if row==n:
             temp=[]
             for r in board:
@@ -21,17 +7,23 @@ class Solution:
             ans.append(temp)
             return
         for j in range(n):
-            if column[j]==0 and self.check(n,board,row,j):
+            if column[j]==0 and leftDig[n-1+j-row]==0 and rightDig[row+j]==0:
                 column[j]=1
                 board[row][j]='Q'
-                self.find(row+1,n,ans,board,column)
+                leftDig[n-1+j-row]=1
+                rightDig[row+j]=1
+                self.find(row+1,n,ans,board,column,leftDig,rightDig)
                 column[j]=0
                 board[row][j]='.'
+                leftDig[n-1+j-row]=0
+                rightDig[row+j]=0
 
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans=[]
         board=[["."] * n for _ in range(n)]
-        column=[False]*n
-        self.find(0,n,ans,board,column)
+        column=[0]*n
+        leftDig=[0]*(2*n-1)
+        rightDig=[0]*(2*n-1)
+        self.find(0,n,ans,board,column,leftDig,rightDig)
         return ans
 
