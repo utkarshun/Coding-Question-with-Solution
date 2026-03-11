a0629e311af6b316549c4358d53b29ab415663e7
Utkarsh kher <kherutkarsh54@gmail.com>
2026-03-11 08:40:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (56.76%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[-1]*n
+        def house(ind):
+            if ind==0:
+                return nums[ind]
+            if ind<0:
+                return 0
+            if dp[ind]!=-1:
+                return dp[ind]
+            left=house(ind-1)
+            right=house(ind-2)+nums[ind]
+            dp[ind]=max(left,right)
+            return dp[ind]
+        return house(n-1)
+
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         n=len(nums)
+#         dp=[-1]*n
+#         dp[0]=nums[0]
+#         neg=0
+#         for i in range(1,len(nums)):
+#             take=nums[i]
+#             if(i>1):
+#                 take+=dp[i-2]
+#             nottake=0+dp[i-1]
+#             dp[i]=max(take,nottake)
+#         return dp[n-1]
