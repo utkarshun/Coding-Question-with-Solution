098cdd7aea39cbad764cf424c0b8e8a7579e1fd3
Utkarsh kher <kherutkarsh54@gmail.com>
2026-03-12 11:05:09 +0530

    Time: 27 ms (30.24%), Space: 20.6 MB (18.29%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,8 +1,16 @@
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         finalposition=len(nums)-1
+#         n=len(nums)
+#         for i in range(n-2,-1,-1):
+#             if(i+nums[i]>=finalposition):
+#                 finalposition=i
+#         return finalposition==0
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        finalposition=len(nums)-1
-        n=len(nums)
-        for i in range(n-2,-1,-1):
-            if(i+nums[i]>=finalposition):
-                finalposition=i
-        return finalposition==0
+        maxReach=0
+        for i in range(len(nums)):
+            if i>maxReach:
+                return False
+            maxReach=max(maxReach,i+nums[i])
+        return True
