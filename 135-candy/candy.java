0be06dc32df3e4accf13159b95278f9998b4d730
Utkarsh kher <kherutkarsh54@gmail.com>
2026-03-14 16:13:53 +0530

    Time: 3 ms (86.07%) | Memory: 47.7 MB (67.25%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,29 +1,59 @@
+// class Solution {
+//     public int candy(int[] ratings) {
+//         int n=ratings.length;
+//         int[] left=new int[n];
+//         int[] right=new int[n];
+//         left[0]=1;
+//         right[n-1]=1;
+//         for(int i=1;i<n;i++){
+//             if(ratings[i]>ratings[i-1]){
+//                 left[i]=left[i-1]+1;
+//             }
+//             else{
+//                 left[i]=1;
+//             }
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             if(ratings[i]>ratings[i+1]){
+//                 right[i]=right[i+1]+1;
+//             }
+//             else{
+//                 right[i]=1;
+//             }
+//         }
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum=sum+Math.max(left[i],right[i]);
+//         }
+//         return sum;
+//     }
+// }
 class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
-        int[] left=new int[n];
-        int[] right=new int[n];
-        left[0]=1;
-        right[n-1]=1;
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
+        int i=1;
+        int sum=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
             }
-            else{
-                left[i]=1;
+            int peak=0;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak+1;
+                i++;
             }
-        }
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+            int down=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                down++;
+                sum+=down;
+                i++;
             }
-            else{
-                right[i]=1;
+            if(down>peak){
+                sum+=down-peak;
             }
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum=sum+Math.max(left[i],right[i]);
         }
         return sum;
     }
